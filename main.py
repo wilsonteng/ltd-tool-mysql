02ab1204b9a575e927f0d68a86036753f3b4f119
wilsonteng <wteng33@gmail.com>
2023-11-21 19:25:18 -1000

    Updates

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ import mysql.connector
 import os
 import logging
 import time
+from pathlib import Path
 
 load_dotenv()
-
 ltd_api_key = os.getenv("ltd_api_key")
-
 mysql_config = {
     'user': os.getenv("mysql_user"),
     'password': os.getenv("mysql_password"),
@@ -20,24 +19,26 @@ mysql_config = {
     'database': os.getenv("mysql_database"),
     'raise_on_warnings': True
     }
-    
-# Set up logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-# Log to console
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-# Also log to a file
-file_handler = logging.FileHandler("cpy-errors.log")
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler) 
 
 def connect_to_mysql(config, attempts=3, delay=2):
     attempt = 1
+
+    # Set up logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    # Log to console
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    # Also log to a file
+    file_handler = logging.FileHandler("cpy-errors.log")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler) 
+
     # Implement a reconnection routine
     while attempt < attempts + 1:
         try:
@@ -58,26 +59,73 @@ def connect_to_mysql(config, attempts=3, delay=2):
             attempt += 1
     return None
 
-cnx = connect_to_mysql(mysql_config, attempts=3, delay=2)
-print(cnx)
-
-
-def api_request() -> dict:
+def make_api_request(offset : int, queuetype : str) -> list:
     """
-    Makes the API request to legion TD api
+    Makes the API request to Legion TD API
     Returns a dictionary containing the data from api call
     """
 
-def check_if_data_useful() -> bool:
+    headers = {'x-api-key': ltd_api_key, 'accept': 'application/json'}
+
+    URL = f"""{ltd_api_key}/games?limit={limit}&offset={offset}&sortBy=date&sortDirection=1&includeDetails=true&countResults=false&queueType={queuetype}"""
+
+    r = requests.get(URL, headers=headers)
+    if not r.ok:
+        print(r, "Response not OK")
+        return False
+    
+    print(f"Retrieving data for {offset} to {offset + int(limit)} of {limit * end} for queueType {queuetype}. Status Code: {r.status_code}")
+
+    return r.text
+
+def check_if_data_useful(input_data) -> bool:
     """
     Checks if this particular build is a pro leak
     Looking for groups of waves that leak less than 4, 4, 7 on waves 1, 2, 3
     """
+    if len(input_data) < 3:
+        return False
 
-def shorten_data() -> dict:
+    if (0 < len(input_data[0]) < 4 and
+        0 < len(input_data[1]) < 4 and
+        0 < len(input_data[2]) < 7):
+            return True
+
+def clean_data(raw_data : str) -> list:
     """
-    After finding a build of interest, truncate data to only waves 1 to 3.
+    Takes in entire api return in string format.
+    Drops columns from both the outer match dictionary and the nested playersData dictionary
+    Returns the updated dictionary
     """
+
+    number_of_waves_to_keep = 3
+    data_dict = raw_data
+    new_list = []
+
+    for game in data_dict:
+        for player in game:
+            for build in player["playersData"]["leaksPerWave"]:
+                if check_if_data_useful(build):
+                    # creating a new dictionary with only the data we need
+                    player_dict = {}
+                    player_dict["id"] = game["_id"] + player["playerSlot"]
+                    player_dict["version"] = game["version"]
+                    player_dict["date"] = game["date"]
+                    player_dict["legion"] = player["legion"]
+                    player_dict["buildPerWave"] = player["buildPerWave"][:3]
+                    player_dict["mercenariesReceivedPerWave"] = player["mercenariesReceivedPerWave"][:3]
+                    player_dict["leaksPerWave"] = player["leaksPerWave"][:3]
+
+                    new_list.append(player_dict)
+    
+    return new_list
+
+with open(/home/wilson/git/legiontd_tool/data/json_data_20231113-150304.json, "r") as f:
+    json_data = json.loads(f.read())
+
+print(clean_data(json_data))
+
+
 
 def write_sql_insert_statement() -> str:
     """
@@ -93,5 +141,24 @@ def insert_into_mysql():
 
 # close cursor
 
+#cnx = connect_to_mysql(mysql_config, attempts=3, delay=2)
+#print(cnx)
+
 
 # Close mysql connection
+
+def main():
+    
+    big_array = [] # make one big array before inserting into sql
+    start, end = 0, 2 #1000 x 50 is the max per day
+    limit = 50
+
+    for i in range(start, end):
+        offset = i * int(limit)
+        data_normal = make_api_request(offset, "Normal")
+        data_classic = make_api_request(offset, "Classic")
+        if not data_normal or not data_classic: 
+            print("Error!")
+            break # break if bad http request so big_array still gets written
+        big_array.extend(clean_data(data_normal))
+        big_array.extend(clean_data(data_classic))
